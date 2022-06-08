612180f35a8196d75b25d518f791e8e4375d52c7
yeabsira <yshewa84@gmail.com>
2022-06-08 08:41:52 -0700

    Write a program that prints

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - prints the string "_putchar" from a character array.
+ * main - prints the string '_putchar' from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "_putchar";
+	char c[] = '_putchar';
 	int i;
 
 	for (i = 0; i < 9; i++)
