ac389591ce3bf129b1feaf77f725378cd49f6312
yeabsira <yshewa84@gmail.com>
2022-06-08 14:00:38 -0700

    There are only 3 colors

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,10 +4,14 @@
  * @n: digit to find the last place of.
  * Return: The last digit.
  */
-int print_last_digit(int n)
+int print_last_digit(int);
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+    int r;
+
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
 }
