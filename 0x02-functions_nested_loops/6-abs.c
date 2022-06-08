81ed109884e9ca46b0883d0ebf517615226bac98
yeabsira <yshewa84@gmail.com>
2022-06-08 13:47:12 -0700

    Write a function that computes the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,29 +1,12 @@
 #include "main.h"
 /**
- * print_sign - Determines whether an integer is positive, negative or zero.
- * @n: the number to be checked.
- * Return: 1 if positive, 0 if zero, -1 if negative, / if not a digit.
+ * _abs - calculate the absolute value of the number from zero.
+ * @i: the integer to be converted.
+ * Return: absolute value of the input number.
  */
-int print_sign(int n)
+int _abs(int i)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	else
-	{
-		_putchar('-');
-		return ('/');
-	}
+	if (i < 0)
+		i = i * -1;
+	return (i);
 }
