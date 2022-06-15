efe2ee85491fa3d181d03065212d061124ec226d
yeabsira <yshewa84@gmail.com>
2022-06-15 13:18:27 -0700

    task-5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,31 +1,20 @@
 #include "main.h"
-
 /**
- * cap_string - Function that reverses the content of an array of integers.
+ * string_toupper - Function that reverses the content of an array of integers.
  *
  * @s: s is the array
  *
  * Return: Always 0.
  */
-char *cap_string(char *s)
+char *string_toupper(char *s)
 {
 	int i = 0;
 
 	while (s[i] != '\0')
 	{
-		if (s[0] <= 122 && s[0] >= 97)
+		if ((s[i] >= 97) && (s[i] <= 122))
 		{
-			s[0] = s[0] - 32;
-		}
-		if (s[i] == 32 || s[i] == 46 || s[i] == '\t' ||
-			s[i] == '\n' || s[i] == 44 || s[i] == 59 ||
-				s[i] == '!' || s[i] == '?' || s[i] == '(' ||
-				s[i] == ')' || s[i] == '{' || s[i] == '}')
-		{
-			if (s[i + 1] <= 122 && s[i + 1] >= 97)
-			{
-				s[i + 1] = s[i + 1] - 32;
-			}
+			s[i] = s[i] - 32;
 		}
 	i++;
 	}
