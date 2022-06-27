3690c8e6962592cdba7c0369efa16e6dc8c9b894
yeabsira <yshewa84@gmail.com>
2022-06-27 12:16:00 -0700

    what

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - the length of a string
+ * @s: is a pointer int char type
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int u;
+	for (u = 0; *s != '\0'; u++)
+		s++;
+	return (u);
+}
